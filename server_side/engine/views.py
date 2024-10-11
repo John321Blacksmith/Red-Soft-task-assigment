2f6b01b10621a28ee934bd6329219eba04f628be
John321Blacksmith <kuzmichevi321@gmail.com>
2024-10-12 01:22:24 +0300

    handlers improved

--- server_side/engine/views.py
+++ server_side/engine/views.py
@@ -12,28 +12,43 @@ class VMManager:
     socket-клиент взаимодействует с 
     хранилищем.
     """
-    def __init__(self):
+    def __init__(self, **conf):
+        self.db_manager = DBManager(**conf)
         self.connected_vms: dict[str, VirtualMachine] = {}
         self.authorized_vms: dict[str, VirtualMachine]= {}
 
-    async def add_new_vm(self, **body) -> bool:
-        return body
+    async def setup_storage(self):
+        result = await self.db_manager.create_db()
+        if result:
+            result = await self.db_manager.create_tables()
+            return result
 
-    async def list_connected_vms(self) -> List[VirtualMachine]:
-        return 'Listing the connected vms'
+    async def add_new_vm(self, **body) -> bool:
+        return await self.db_manager.create_vm(**body)
+    
+    async def add_new_hd(self, vm_id: int, **body) -> bool:
+        return await self.db_manager.create_hd(vm_id, **body)
+
+    async def list_vms(self) -> List[VirtualMachine]:
+        return await self.db_manager.select_vms()
 
     async def list_authorized_vms(self) -> List[VirtualMachine]:
-        return 'list_authorized_vms'
+        return self.authorized_vms
 
-    async def list_ever_connected_vms(self) -> List[VirtualMachine]:
-        return 'list_ever_connected_vms'
+    async def list_connectable_vms(self) -> List[VirtualMachine]:
+        return await self.db_manager.select_connectable_vms()
 
     async def logout_vm(self, pk: str) -> bool:
-        del self.authorized_vms[pk]
+        if pk in self.authorized_vms:
+            del self.authorized_vms[pk]
         return pk in self.authorized_vms
 
     async def list_hard_drives(self) -> List[HardDrive]:
-        return 'list_hard_drives'
+        return await self.db_manager.select_hard_drives()
 
-    async def update_vm_data(self) -> VirtualMachine:
-        return 'update_vm_data'
+    async def update_vm_data(self, vm_id: int, **body) -> VirtualMachine:
+        return await self.db_manager.update_vm(vm_id, **body)
+    
+    async def list_test_data(self):
+        result = await self.db_manager.select_test_data()
+        return [{'f_name': dict(r)['first_name']} for r in result]
