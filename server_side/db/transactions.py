4d0d1c959599b135b5cacb916ad1061ebb0b59df
John321Blacksmith <kuzmichevi321@gmail.com>
2024-10-18 13:36:59 +0300

    fixed returning messages

--- server_side/db/transactions.py
+++ server_side/db/transactions.py
@@ -29,7 +29,7 @@ class DBManager:
         """
         ...
     
-    def transaction(self, funct):
+    def transaction(funct):
         """
         Проверка процесса транзакции на исключенияю
         """
